a985e0cb8595ff5163ccb684af2949440dcde9cd
Marshall Slemp <marshall.slemp@gmail.com>
2018-07-22 12:53:42 -0500

    add trim() and toLowerCase()

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,34 +7,36 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-
 // pigLatin - takes a word, moves all letters, up to the first vowel, to the end and adds 'ay'
 // example pigLatin('tiger') => returns 'igertay'
 // steps:
 /*
-  1) Check for valid input - truthy, string
-    a) If truthy value, convert input to String(input) -
-        should work for strings and numbers now.
+  1) Check for valid input - truthy, trim whitespace, make all lower case, turn non-strings to strings
   2) Create an array that holds vowels = ['a','e','i','o','u'].
-  3) Get index of first vowel in word using indexOf, store in variable named index.
+  3) Get index of first vowel in word using indexOf, store in variable named index. if index = 0, return word + 'yay'
   4) Get letters before first vowel and store in variable named firstLetters, using string.slice(0, index).
   5) Get letters after first vowel(inclusive) and store in variable named lettersAfter, using string.slice(index)
   6) Using concatenation, return lettersAfter + firstLetters + 'ay';
+  METHODS: String(), trim(), toLowerCase(), indexOf(), For Loop, 
 */
+
 const pigLatin = (word) => {
   if(word){
-    const input = String(word);
+    const input = String(word).trim().toLowerCase();
     const vowels = ['a','e','i','o','u'];
     let index;
     for(let i = 0; i < input.length; i++){
       if(vowels.indexOf(input[i]) > -1){
         let firstVowelIndex = vowels.indexOf(input[i]);
         index = input.indexOf(vowels[firstVowelIndex]);
+        if(index === 0){
+          return `${input}yay`;
+        }
         break;
       }
     }
     const firstLetters = input.slice(0, index || 0);
-    const lettersAfter = input.slice(index || 0);
+    const lettersAfter = input.slice(index);
     return `${lettersAfter}${firstLetters}ay`;
   }
 }
