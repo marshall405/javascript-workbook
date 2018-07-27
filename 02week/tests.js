331a7f7d6f7847329c4077bdc7da3f5504635f26
Marshall Slemp <marshall.slemp@gmail.com>
2018-07-27 00:11:52 -0500

    remove extra code

--- 02week/tests.js
+++ 02week/tests.js
@@ -1,11 +1,6 @@
 'use strict';
 
 const assert = require('assert');
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
 const formatHand = (hand) => {
     let setHand;
