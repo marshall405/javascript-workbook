8c856746ef02130f1b33a884d16252f5989bdf4b
Marshall Slemp <marshall.slemp@gmail.com>
2018-08-02 00:02:29 -0500

    add valid input, and tests

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,48 +19,62 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-
-function isLegal(startStack, endStack) {
-  // Your code here
-  const startingStack = stacks[startStack],
-        endingStack   = stacks[endStack];
-  return startingStack[startingStack.length - 1] < endingStack[endingStack.length - 1] || endingStack.length == 0;
-}
-
-function movePiece(startStack, endStack) {
-  // Your code here
-    const removedItem = stacks[startStack].pop();
-    stacks[endStack].push(removedItem);
-}
-
-function checkForWin() {
-  // Your code here
-  return stacks.a.length === 0 && (stacks.b.length === 0 || stacks.c.length === 0);
-}
 // towersOfHanoi takes two arguments, startStack and endStack, and pops() the last item from startStack and push() to endStack if move is legal
+// Check if input is valid
+//    -isValidInput()
+//      takes two string arguments
+//      check that the inputs are 'a', 'b' or 'c'
 // Check if move is legal
 //    -isLegal() 
 //      takes two arguments,
 //      checks if the last item on startStack is less than last item on endStack, if true move piece
 // if legal, move piece
 //    -movePiece()
-//      takes two arguments
+//      takes two arguments, startStack and endStack
 //      moves last item on startStack to endStack 
 //      methods: pop() and push()
 // After move, check for win
 //    -checkForWin()
 //      takes 0 arguments
 //      check if all items are on the last stack
-//      check for empty arrays on first two
-function towersOfHanoi(startStack, endStack) {
+//      check the that the first array is empty and either the second or third array is empty
+
+const isValidInput = (startStack, endStack) => {
+  const validInputsArr = ['a','b','c'];
+  return (validInputsArr.indexOf(startStack) != -1) && (validInputsArr.indexOf(endStack) != -1);
+}
+
+const isLegal = (startStack, endStack) => {
   // Your code here
-  if(isLegal(startStack, endStack)){
-    movePiece(startStack, endStack);
-    if(checkForWin()){
-      console.log('Winner');
+  const startingStack = stacks[startStack];
+  const endingStack = stacks[endStack];
+  return startingStack[startingStack.length - 1] < endingStack[endingStack.length - 1] || endingStack.length == 0;
+}
+
+const movePiece = (startStack, endStack) => {
+  // Your code here
+    const removedItem = stacks[startStack].pop();
+    stacks[endStack].push(removedItem);
+}
+
+const checkForWin = () => stacks.a.length === 0 && (stacks.b.length === 0 || stacks.c.length === 0);
+
+
+const towersOfHanoi = (startStack, endStack) => {
+  // Your code here
+  const startStackFormatted = startStack.trim().toLowerCase();
+  const endStackFormatted = endStack.trim().toLowerCase();
+  if(isValidInput(startStackFormatted, endStackFormatted)){
+    if(isLegal(startStackFormatted, endStackFormatted)){
+      movePiece(startStackFormatted, endStackFormatted);
+      if(checkForWin()) {
+        console.log('Winner');
+      }
+    } else {
+      console.log('Invalid Move')
     }
   } else {
-    return 'invalid move';
+    console.log('Invalid Move');
   }
 }
 
@@ -84,7 +98,6 @@ if (typeof describe === 'function') {
       assert.deepEqual(stacks, { a: [4, 3, 2], b: [1], c: [] });
     });
   });
-
   describe('#isLegal()', () => {
     it('should not allow an illegal move', () => {
       stacks = {
@@ -109,6 +122,14 @@ if (typeof describe === 'function') {
       assert.equal(checkForWin(), true);
       stacks = { a: [1], b: [4, 3, 2], c: [] };
       assert.equal(checkForWin(), false);
+    });
+  });
+  describe('#isValidInput', () => {
+    it('should only accept a, b or c', () => {
+      assert.equal(isValidInput('a', 'b'), true);
+    });
+    it('should not accept any value other than a, b or c', () => {
+      assert.equal(isValidInput('notAValidInput', 'a'), false);
     });
   });
 
