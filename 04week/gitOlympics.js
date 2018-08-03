0fd8694e5d2bb3b5cdddecaf0e10ce4b4a8454d9
Marshall Slemp <marshall.slemp@gmail.com>
2018-08-02 19:14:40 -0500

    inital commit

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const printListOfOlympians = (arr) => {
+    arr.forEach( (item) => {
+        console.log(item);
+    });
+}
+
+printListOfOlympians(['Levi', 'Alerto', 'Marshall', 'Ricky Bobby', 'Mike']);
+
