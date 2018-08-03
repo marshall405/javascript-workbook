8142e48ef46733766952f0c1c8d5abda1c5ddbdb
Marshall Slemp <marshall.slemp@gmail.com>
2018-08-02 19:18:01 -0500

    more comments

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -1,11 +1,12 @@
 'use strict'
 
 // printListOfOlypians takes an array and loops throught, printing each one to the console
+
 const printListOfOlympians = (arr) => {
     arr.forEach( (item) => {
         console.log(item);
     });
 }
-
+// call function
 printListOfOlympians(['Levi', 'Alerto', 'Marshall', 'Ricky Bobby', 'Mike']);
 
