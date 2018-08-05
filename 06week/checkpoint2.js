21c5d22eeb20ade9f268736297e93d4d240e4411
Marshall Slemp <marshall.slemp@gmail.com>
2018-08-05 09:31:53 -0500

    work on bubble sort

--- 06week/checkpoint2.js
+++ 06week/checkpoint2.js
@@ -102,3 +102,25 @@ console.log(weatherStates);
 // const idealTemp = weather[3]['id'];
 const idealTemp = weather.find( item => item.min_temp == 15.915) ? weather.find( item => item.min_temp == 15.915).id : 'The ideal temperature does not exist';
 console.log(idealTemp)
+
+
+
+
+//  Bubble Sort - NOT PART OF THE CHECKPOINT - DELETE BEFORE SUBMITTING!!!!!!!!!!!!!!!!!!!
+const bubbleSort = (arr) => {
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] > arr[i + 1]){
+      let placeholder = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = placeholder;
+    }
+  }
+  arr.forEach( (item, index, array) => {
+    if(item > array[index + 1]){
+      bubbleSort(arr);
+    }
+  });
+  return arr;
+}
+console.log(bubbleSort(nums));
+
