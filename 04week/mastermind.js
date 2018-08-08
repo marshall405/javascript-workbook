440b5ac34c456e670c3d65a8f95969457867de50
Marshall Slemp <marshall.slemp@gmail.com>
2018-08-07 22:47:44 -0500

    used for practice

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -11,7 +11,10 @@ let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
+
+
 function printBoard() {
+  console.log(solution);
   for (let i = 0; i < board.length; i++) {
     console.log(board[i]);
   }
@@ -28,21 +31,66 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function generateHint() {
+function generateHint(guess) {
   // your code here
+  const solutionArray = solution.split('');
+  const guessArray = guess.split('');
+  let correctLettersLocations = 0;
+  for(let i = 0; i < solutionArray.length; i++){
+    if(solutionArray[i] === guessArray[i]){
+      correctLettersLocations++;
+      solutionArray[i] = null;
+    }
+  }
+  let correctLetters = 0;
+  for(let i = 0; i < solutionArray.length; i++){
+    let targetIndex = solutionArray.indexOf(guessArray[i]);
+    if(targetIndex > -1){
+      correctLetters++;
+      solutionArray[targetIndex] = null;
+    }
+  }
+  return `${correctLettersLocations}-${correctLetters}`;
 }
-
+const startNewGame = () => {
+  rl.question('Play again? (y/n)', (input) => {
+    if(input === 'y'){
+      board = [];
+      solution = '';
+      generateSolution();
+      getPrompt();
+    } else{
+      console.log('Have a great day!');
+      rl.close();
+    }
+  });
+}
 function mastermind(guess) {
-  solution = 'abcd'; // Comment this out to generate a random solution
+  // solution = 'abdc'; // Comment this out to generate a random solution
   // your code here
+  if(guess && typeof guess === 'string' && guess.length === solution.length){
+    if(guess === solution){
+      console.log('You guessed it!');
+      startNewGame();
+    } else if(board.length >= 10){
+      console.log(`You ran out of turns! The solution was ${solution}`);
+      startNewGame();
+    } else {
+      console.log('Guess again.');
+      let hint = `${guess} ${generateHint(guess)}`
+      board.push(hint);
+    }
+  }
 }
 
 
 function getPrompt() {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
-    printBoard();
-    getPrompt();
+    if(guess != solution || board.length < 11){
+      printBoard();
+      getPrompt();
+    }
   });
 }
 
